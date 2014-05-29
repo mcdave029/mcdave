3f053f1edda9dc195aa2c23f32b84d4ba18a7964
Mc Dave <mdvaldecantos@gmail.com>
2014-05-29 13:52:31 +0800

    My first ROR!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcdave::Application.config.secret_token = '52d145d0d44cb8515eee1a6ec7f3b5566cec6d372ab5569eb36122b65edec4ba8fe14dd0b376a7712609c7905bc4a5c5fdd8f6e52726bfac81bb060ceac61175'
